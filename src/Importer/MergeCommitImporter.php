e8af451f19649c151b9dd7bef706b9adf1ad342e
IVI <poulai_a@etna-alternance.net>
2019-10-10 13:59:38 +0200

    fix

--- src/Importer/MergeCommitImporter.php
+++ src/Importer/MergeCommitImporter.php
@@ -4,21 +4,13 @@ namespace App\Importer;
 
 use App\Helper\githubHelper;
 use App\Converter\MergeCommitConverter;
-use Doctrine\ORM\EntityManagerInterface;
 
 class MergeCommitImporter
 {
-    protected $em;
-
-    public function __construct(EntityManagerInterface $entityManager)
-    {
-        $this->em = $entityManager;
-    }
-
     public function import()
     {
         $helper = new githubHelper();
-        $converter = new MergeCommitConverter($this->em);
+        $converter = new MergeCommitConverter();
         $githubResponse = $helper->callGithub();
         return $converter->convert($githubResponse);
     }
